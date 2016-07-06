3e1aca299c24565118f20c0ed6b1809091936f26
JS <Jeremiah@Jertop>
2016-07-05 21:44:03 -0600

    Added EPBFile Class

---
+++ EGS BP Editor/src/bp/EPBFile.java
@@ -0,0 +1,25 @@
+package bp;
+
+public class EPBFile {
+	MetaData metaTemp = new MetaData();
+	private BlockData blocks;
+	private MetaData meta;
+	
+	public EPBFile(BlockData b, MetaData m) {
+		blocks = b;
+		meta = m;
+	}
+	
+	public EPBFile(byte[] dataBuf, byte[] metaBuf) {
+		meta = new MetaData();
+		blocks = new BlockData();
+
+		meta.populate(metaBuf);
+		blocks.populate(dataBuf);
+	}
+
+	public ZipFile getZip() {
+		return new ZipFile(meta, blocks);
+	}
+
+}
